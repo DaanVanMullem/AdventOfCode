812533fdae8a824e1d141c755f46b9cdf5e52a3c
DaanVanMullem <Daano9140@gmail.com>
2024-12-02 20:11:32 +0100

    Day 1 Finished

---
+++ AdventOfCode/AdventOfCode/Day1/Day1.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode;
+
+public class Day1
+{
+
+    private const string File = "C:/Personal/AdventOfCode/AdventOfCode/AdventOfCode/Day1/Day1_1_input.txt";
+
+    public void Challenge1()
+    {
+        List<int> inputA = [];
+        List<int> inputB = [];
+        int distanceCount = 0;
+
+        foreach (var line in System.IO.File.ReadLines(File))
+        {
+            var parsed = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            inputA.Add(int.Parse(parsed[0])); 
+            inputB.Add(int.Parse(parsed[1]));
+        }
+
+        inputA = inputA.OrderBy(n => n).ToList();
+        inputB = inputB.OrderBy(n => n).ToList();
+
+        for (int i = 0; i < inputA.Count; i++)
+        {
+            int distance = Math.Abs(inputA[i] - inputB[i]);
+            distanceCount += distance;
+        }
+        
+        Console.WriteLine($"Day 1 Challenge 1: {distanceCount}");
+    }
+    
+    public void Challenge2()
+    {
+        List<int> inputA = [];
+        List<int> inputB = [];
+        int distanceCount = 0;
+
+        foreach (var line in System.IO.File.ReadLines(File))
+        {
+            var parsed = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            inputA.Add(int.Parse(parsed[0])); 
+            inputB.Add(int.Parse(parsed[1]));
+        }
+
+        foreach (var value in inputA)
+        {
+            distanceCount += (value * inputB.Count(n => n == value));
+        }
+        
+        Console.WriteLine($"Day 1 Challenge 2: {distanceCount}");
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Day 1
+        Day1 day1 = new Day1();
+        day1.Challenge1();
+        day1.Challenge2();
+
+        
+    }
+}
