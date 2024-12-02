91d30a637865e846c3f8edf44efbe4d77e9d18c6
DaanVanMullem <Daano9140@gmail.com>
2024-12-02 23:50:47 +0100

    Day 2 1

---
+++ AdventOfCode/AdventOfCode/Day2/Day2.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode;
+using System;
+
+public class Day2
+{
+    private const string File = "C:/Personal/AdventOfCode/AdventOfCode/AdventOfCode/Day2/Day2_input.txt";
+
+    public void Challenge1()
+    {
+        int[][] input = System.IO.File.ReadLines(File).Select(n => Array.ConvertAll(n.Split(' '), int.Parse)).ToArray();
+        int safeCount = 0;
+        
+        for (int i = 0; i < input.Length; i++)
+        {
+            int prevValue = new int();
+            bool ascending = false;
+            bool safe = true;
+
+            for (int j = 0; j < input[i].Length; j++)
+            {
+                if (j == 0)
+                {
+                    prevValue = input[i][j];
+                    if (input[i][j+1] == prevValue)
+                    {
+                        safe = false;
+                        break;
+                    }
+                    ascending = (input[i][j+1] > prevValue);
+                    continue;
+                }
+
+                var test = input[i][j];
+                if (ascending)
+                {
+                    if (input[i][j] > prevValue && (input[i][j] - prevValue) <= 3)
+                    {
+                        prevValue = input[i][j];
+                        continue;
+                    }
+                }
+                if (!ascending)
+                {
+                    if (input[i][j] < prevValue && (prevValue - input[i][j]) <= 3)
+                    {
+                        prevValue = input[i][j];
+                        continue;
+                    }
+                }
+                safe = false;
+                break;
+            }
+            if (safe) safeCount++;
+        }
+        Console.WriteLine($"Day 2 Challenge 1: {safeCount}");
+    }
+
+    public void Challenge2()
+    {
+        
+    }
+    
+}
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -5,10 +5,14 @@ class Program
     static void Main(string[] args)
     {
         //Day 1
-        Day1 day1 = new Day1();
-        day1.Challenge1();
-        day1.Challenge2();
-
+        // Day1 day1 = new Day1();
+        // day1.Challenge1();
+        // day1.Challenge2();
         
+        //Day 2
+        Day2 day2 = new Day2();
+        day2.Challenge1();
+        day2.Challenge2();
+
     }
 }
